f848dc4b3794e3ee1b31fc6025d73ae4d3ebf0a3
billy <Billy@mask.io>
2022-06-16 19:07:21 +0800

    feat: lock item

--- src/views/TimeLock/NewLock.tsx
+++ src/views/TimeLock/NewLock.tsx
@@ -4,35 +4,10 @@ import styled from 'styled-components'
 import { TimeLockIcon, Input } from '@pancakeswap/uikit'
 import { AppHeader, AppBody } from '../../components/App'
 import { DatePicker } from 'views/Voting/components/DatePicker'
+import TimeCards from './components/TImeCards'
 
 const Body = styled(CardBody)`
   background-color: ${({ theme }) => theme.colors.backgroundAlt2};
-`
-
-const Row = styled.div`
-  width: 100%;
-  margin-bottom: 1rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 2rem;
-`
-
-const TimeItem = styled.div`
-  margin-top: 12px;
-  background: #ffffff;
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.125);
-  border-radius: 8px;
-  padding: 16px 12px;
-  box-sizing: border-box;
-  font-size: 2rem;
-  color: rgba(13, 166, 234, 1);
-  margin-bottom: 16px;
-`
-const TimeText = styled.div`
-  color: rgba(0, 123, 228, 1);
-  font-weight: 400;
-  font-size: 16;
 `
 
 const Intro = styled.div`
@@ -83,14 +58,7 @@ export default function NewLock() {
             selected={lockTime}
             placeholderText="YYYY/MM/DD/HH"
           />
-          <Row>
-            {remainTime.map((x, idx) => (
-              <Flex flexDirection={'column'} alignItems="center">
-                <TimeItem>{x}</TimeItem>
-                <TimeText>{['DAYS', 'HOURS', 'MINS'][idx]}</TimeText>
-              </Flex>
-            ))}
-          </Row>
+         <TimeCards remainTime={remainTime} />
         </Flex>
       </Body>
       <CardFooter style={{ textAlign: 'center' }}>
--- src/views/TimeLock/components/LockItem.tsx
+++ src/views/TimeLock/components/LockItem.tsx
@@ -1,6 +1,10 @@
 import { TokenImage } from 'components/TokenImage'
 import styled from 'styled-components'
 import { WETH } from '@pancakeswap/sdk'
+import { TimeLockIcon } from '@pancakeswap/uikit'
+import TimeCards from './TImeCards'
+import { useState } from 'react'
+import useTheme from 'hooks/useTheme'
 
 const Wrapper = styled.div`
   width: 360px;
@@ -11,6 +15,7 @@ const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.card.background};
   border-radius: 35px;
   box-sizing: border-box;
+  box-shadow: 0px 4px 8px rgba(0, 123, 228, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25);
 `
 const InfoHeader = styled.div`
   width: 100%;
@@ -33,8 +38,27 @@ const InfoLeft = styled.div`
   color: ${({ theme }) => theme.colors.silver};
 `
 
+const InfoStatus = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.primary};
+  font-weight: 500;
+  gap: 16px;
+  margin: 8px 0;
+`
+const InfoItem = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 12px 0;
+`
+
 export function LockItem() {
+  const [remainTime, setRemainTime] = useState(['00', '00', '00'])
   const token = WETH[321]
+  const color = useTheme()
   return (
     <Wrapper>
       <InfoHeader>
@@ -45,6 +69,21 @@ export function LockItem() {
         </InfoLeft>
         <TokenImage width={48} height={48} token={token} />
       </InfoHeader>
+      <InfoStatus>
+        <TimeLockIcon width={24} height={24} />
+        <span>进行中</span>
+      </InfoStatus>
+      <div style={{ width: '100%', marginBottom: '48px' }}>
+        <TimeCards remainTime={remainTime} />
+      </div>
+      <InfoItem>
+        <div>锁仓</div>
+        <div> 1644222 MDAO</div>
+      </InfoItem>
+      <InfoItem style={{ color: color.theme.colors.primary }}>
+        <div>锁仓时间</div>
+        <div> 16天06小时32分 </div>
+      </InfoItem>
     </Wrapper>
   )
 }
